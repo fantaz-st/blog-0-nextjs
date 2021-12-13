8b59e814b94bd843e6c66f704d1e7f6a5efa1cf4
fantaz-st <cbabic@pfst.hr>
2021-12-13 18:46:52 +0100

    Changed the avatar image

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={classes.hero}>
       <div className={classes.image}>
-        <Image src="/images/site/kometi0.jpg" alt="An image showing fantaz" width={300} height={300} />
+        <Image src="/images/site/home_avatar.jpeg" alt="An image showing fantaz" width={300} height={300} />
       </div>
       <h1>Hi, I'm Fantaz</h1>
       <p>I develop the internet, with help from frontend frameworks like NextJS.</p>
